9fa449a1d640a21c92cea2af622c08dec287a996
shadowbox <shadowboxx@hotmail.com>
2023-11-04 11:45:40 +0800

    customer

--- api.sh
+++ api.sh
@@ -1,1 +1,1 @@
-python3 openai_api.py --checkpoint-path finetune/calf8414/output-20231028/checkpoint-1000
+python3 openai_api.py --checkpoint-path finetune/calf8414/output-20231102
--- cli.sh
+++ cli.sh
@@ -1,1 +1,1 @@
-python3 cli_demo.py --checkpoint-path finetune/calf8414/output-20231028/checkpoint-1000
+python3 cli_demo.py --checkpoint-path finetune/calf8414/output-20231102
